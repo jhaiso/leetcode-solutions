981618864a5c70d57a473eaf6ec0a2fdf129e8a3
jhaiso <jeffreyhaisong@gmail.com>
2025-06-18 22:53:03 -0400

    Invert Binary Tree

---
+++ invert-binary-tree/details.md
@@ -0,0 +1,11 @@
+# 226. Invert Binary Tree
+
+## Relevant Topics
+
+Binary Tree, DFS, BFS
+
+## Solution Thought Process
+### BFS/DFS
+We see that inverting the tree involved switching the left and right children of each node. We also see that the order does not matter!
+Time Complexity: O(n)
+Space Complexity: O(n)
---
+++ invert-binary-tree/invert_binary_tree.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution_BFS_Recursive:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return root
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
+    
+
+class Solution_DFS_Iterative:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return root
+        stack = [root]
+        while stack:
+            curr = stack.pop()
+            curr.left, curr.right = curr.right, curr.left
+            if curr.left:
+                stack.append(curr.left)
+            if curr.right:
+                stack.append(curr.right)
+        
+        return root
