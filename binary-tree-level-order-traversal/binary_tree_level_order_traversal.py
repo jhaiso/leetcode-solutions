9a55824da179bb1a40ec658396c9a4a8044b35bd
jhaiso <jeffreyhaisong@gmail.com>
2025-06-21 11:27:06 -0400

    Binary Tree Level Order Traversal

---
+++ binary-tree-level-order-traversal/binary_tree_level_order_traversal.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+import collections
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        
+        res = []
+        q = collections.deque([(root, 0)])
+        while q:
+            curr, depth = q.popleft()
+            if depth < len(res):
+                res[depth].append(curr.val)
+            else:
+                res.append([curr.val])
+            if curr.left:
+                q.append((curr.left, depth + 1))
+            if curr.right:
+                q.append((curr.right, depth + 1))
+
+        return res
---
+++ binary-tree-level-order-traversal/details.md
@@ -0,0 +1,6 @@
+# 102. Binary Tree Level Order Traversal
+## Relevant Topics
+Tree, Binary Tree, DFS, BFS
+
+## Solution Thought Process
+We need to keep track of the depth. Let's think recursively.
