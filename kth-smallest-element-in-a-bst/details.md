ecc800ab86fb51cfcf4de8d7ddb53733ccffef28
jhaiso <jeffreyhaisong@gmail.com>
2025-06-21 16:35:53 -0400

    Kth Smallest Element in a BST

--- kth-smallest-element-in-a-bst/details.md
+++ kth-smallest-element-in-a-bst/details.md
@@ -5,4 +5,8 @@
 Tree, Binary Search Tree, Inorder Traversal
 
 ## Solution Thought Process
+### DFS
 In a binary search tree, the smallest value is found by traversing to the left child until the last one. In looking at the example problems and visualizing, we see that from the smallest node, we traverse upwards, and explore the right subtree in the same order when available. Because we are going 'up' the tree, and going left to right, an inorder traversal would be appropriate.
+
+### Morris Traversal
+Because it is an inorder traversal, we can use a Morris Traversal to have a space complexity of O(1)
--- kth-smallest-element-in-a-bst/kth_smallest_element_in_a_bst.py
+++ kth-smallest-element-in-a-bst/kth_smallest_element_in_a_bst.py
@@ -24,3 +24,37 @@ class Solution:
             return left if left else right
         
         return inorder(root)
+    
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def kthSmallest_Morris(self, root: Optional[TreeNode], k: int) -> int:
+        curr = root
+
+        while curr:
+            if not curr.left:
+                k -= 1
+                if k == 0:
+                    return curr.val
+                curr = curr.right
+            else:
+                pred = curr.left
+                while pred.right and pred.right != curr:
+                    pred = pred.right
+                
+                if not pred.right:
+                    pred.right = curr
+                    curr = curr.left
+                else:
+                    pred.right = None
+                    k -= 1
+                    if k == 0:
+                        return curr.val
+                    curr = curr.right
+        
+        return -1
