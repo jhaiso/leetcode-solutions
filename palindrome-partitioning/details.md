616774721252875341401fd29367bb120a3cc6a8
jhaiso <jeffreyhaisong@gmail.com>
2025-06-27 13:11:09 -0400

    Palindrome Partitioning

---
+++ palindrome-partitioning/details.md
@@ -0,0 +1,8 @@
+# 131. Palindrome Partitioning
+
+## Relevant Topics
+
+Backtracking
+
+## Solution Thought Process
+Because we need to come up with ALL possible palindrome partitionings, backtracking comes to mind. At each step, we can either include the current character in the previous group, or create a new one.
---
+++ palindrome-partitioning/palindrome_partitioning.py
@@ -0,0 +1,24 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        curr = []
+        def backtrack(idx):
+            if idx >= len(s):
+                res.append(curr[:])
+                return
+            for i in range(idx, len(s)):
+                if self.isPalindrome(s[idx:i+1]):
+                    curr.append(s[idx:i+1])
+                    backtrack(i+1)
+                    curr.pop()
+        backtrack(0)
+        return res
+
+    def isPalindrome(self, s: str): # can be faster with 2 pointers
+        l, r = 0, len(s)-1
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l+=1
+            r-=1
+        return True
